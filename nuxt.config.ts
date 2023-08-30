b7b5630bbcfd6264ccc1567d5836cf666851e909
now17 <now17@sgrsoft.com>
2023-08-30 16:01:43 +0900

    ...

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,10 @@ export default defineNuxtConfig({
             ],
             script: [
                 { src: 'https://unpkg.com/@phosphor-icons/web' },
-                { src: 'https://player.vpe.naverncp.com/ncplayer.1.1.1.dev.beta.js?access_key=17e49e099ea78c1f1ae159fc1647316c&v=betaV.15' },
+                { src: 'https://player.vpe.naverncp.com/ncplayer.1.1.1.dev.beta.js?access_key=17e49e099ea78c1f1ae159fc1647316c&v=betaV.16' },
+
+
+
 
             ],
         },
