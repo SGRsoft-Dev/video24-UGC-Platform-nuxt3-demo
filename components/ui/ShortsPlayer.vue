f42348a7a67daad2abf40e42f13d8c6d70bc5486
김종태 <now17@sgrsoft.com>
2023-09-09 15:43:33 +0900

    ...

--- components/ui/ShortsPlayer.vue
+++ components/ui/ShortsPlayer.vue
@@ -44,6 +44,8 @@
 
 <script setup>
 
+import axios from "axios";
+
 const props = defineProps({
 	video: {
 		type: Object,
@@ -66,9 +68,50 @@ const props = defineProps({
 		default: 0
 	},
 });
+const runtimeConfig = useRuntimeConfig();
+const mpKey = runtimeConfig.public.mediaPlusApiKey;
 const windowSize = useState('windowSize');
+const UUID = useState('UUID');
 const {$util} = useNuxtApp();
 
+
+/**
+ * VOD 조회수 업데이트
+ * @returns {Promise<void>}
+ */
+const vodViewCountUpdate = async (vod)=>{
+
+	if(!vod) return false;
+
+	let {data} = await axios.post(`https://mediaplus.co.kr/openApi/v1/analytics`,{
+		uid:UUID.value,
+		oid:vod.oid,
+		ovp_channel_id:vod.channel_id,
+		video_id:vod.video_id,
+		type:'vod',
+		currentTime:0,
+		duration:vod.duration,
+		viewingTime:0,
+		percent:0,
+
+
+	},{
+		headers:{
+			'Authorization':mpKey
+		}
+	});
+}
+
+
+onMounted(()=>{
+
+	if(!props.video) return false;
+
+	if(props.active) {
+		vodViewCountUpdate(props.video);
+	}
+
+})
 
 </script>
 
